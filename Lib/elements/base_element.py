a3a3746d60884ba2a7f915ecaceaf9d6ad45b0a7
ofedun <olenka.fedun@gmail.com>
2017-07-19 20:00:33 +0300

    Addressed comments

--- Lib/elements/base_element.py
+++ Lib/elements/base_element.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 TIMEOUT = 10
 
 class BaseElement(object):
-    def __init__(self, driver, element_tuple, timeout=10):
+    def __init__(self, driver, element_tuple, timeout=TIMEOUT):
         self.driver = driver
         self.locator_type = element_tuple[0]
         self.locator = element_tuple[1]
@@ -26,15 +26,13 @@ class BaseElement(object):
         element = self.find_visible_element(self.locator_type, self.locator, self.timeout)
         return element.text
 
-    def find_visible_element(self, locator_type, locator, timeout=None):
+    def find_visible_element(self, locator_type, locator, timeout=TIMEOUT):
         """Find visible element.
 
         Returns:
             element : the WebElement once it is located.
         """
-        wait = WebDriverWait(self.driver, TIMEOUT)
-        # elements = wait.until(EC.presence_of_element_located((By.XPATH, locator)))
-        # my_by_xpath = By.XPATH
+        wait = WebDriverWait(self.driver, timeout)
         find_by_locator = getattr(By, locator_type.upper())
         element = wait.until(EC.presence_of_element_located((find_by_locator, locator)))
         return element
